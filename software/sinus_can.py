d8085c04f1782b29d18ade268758c0f2d6414b98
Thomas Flayols <tflayols@laas.fr>
2023-08-08 12:35:21 +0200

    improve the python script to parse incomming data, and publish them in realtime for plotjuggler

--- software/sinus_can.py
+++ software/sinus_can.py
@@ -1,16 +1,38 @@
-#sudo ip link set can0 type can bitrate 1000000
-#sudo ip link set up can0
-#candump can0 (optional - to see the trafic)
 '''
+tflayols@laas.fr
+
 This code sends positions commands to the motor controller, 
-It doesnt read sensors frame
+The state and commands data are published via UDP in json to be ploted in real time with plotjuggler (optional)
+To plot the data, install and run plotjugler (UDP/JSON  9870)
 
+Note to configure my CAN network interface:
+sudo ip link set can0 type can bitrate 1000000
+sudo ip link set up can0
+candump can0 (optional - to see the trafic)
 '''
-
+import threading
 import can
 import time
 from math import sin, pi
+import struct
+import socket
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+plotjuggler_address = ('localhost', 9870)  # Change as needed
+q_latest = 0
+q_dot_latest = 0
+tau_latest = 0
+lock = threading.Lock()
+
+def receive_state_packet(bus):
+    global q_latest, q_dot_latest, tau_latest
+    while True:
+        message = bus.recv(0) # Non-blocking read
+        if message and message.arbitration_id == 0x456:  
+            with lock:
+                q_latest, q_dot_latest, tau_latest = struct.unpack('<lhh', message.data)
+            print ("packet!")
+        
 def pack_command_buffer(pos_gain, vel_gain, tau_ff, q_dot_d, q_d):
     assert 0 <= pos_gain <= 255
     assert 0 <= vel_gain <= 255
@@ -32,17 +54,25 @@ def pack_command_buffer(pos_gain, vel_gain, tau_ff, q_dot_d, q_d):
 
 # Initialize the CAN bus
 bus = can.interface.Bus(channel='can0', bustype='socketcan')
-
+receive_thread = threading.Thread(target=receive_state_packet, args=(bus,))
+receive_thread.daemon = True
+receive_thread.start()
 
 dt=0.001
 T=10
-f = 0.1
+f = 1
+
 for i in range (int(T/dt)):
     t=i*dt
-    q_d = sin(2*pi*f*t);
-    data = pack_command_buffer(1, 20, 0, 0, int(100000*q_d)) 
+    q_d = int(100000*sin(2*pi*f*t));
+    data = pack_command_buffer(1, 20, 0, 0, q_d)
+    # ~ data = pack_command_buffer(0, 0, 0, 0, q_d)
     message = can.Message(arbitration_id=0x123, data=data, is_extended_id=False)
     bus.send(message)
+    with lock:
+        print (q_d, q_latest, q_dot_latest, tau_latest)
+        msg = '{{"time": {}, "q_d": {}, "q": {}, "q_dot": {}, "tau": {}}}'.format(t, q_d, q_latest, q_dot_latest, tau_latest)
+    sock.sendto(msg.encode('utf-8'), plotjuggler_address) #send to plotjuggler
     time.sleep(dt) #Not taking intoo account the loop execution time here..
 
 bus.shutdown()
